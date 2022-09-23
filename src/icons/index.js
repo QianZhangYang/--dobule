404b94a6f77512de710045035bdc512896ce09ea
张强 <719393762@qq.com>
2022-09-23 19:41:29 +0800

    feat:项目初始化完成

--- src/icons/index.js
+++ src/icons/index.js
@@ -1,9 +1,12 @@
 import Vue from 'vue'
 import SvgIcon from '@/components/SvgIcon'// svg component
-
+// import svg from '@/icons/svg/dashboard.svg'
 // register globally
 Vue.component('svg-icon', SvgIcon)
 
 const req = require.context('./svg', false, /\.svg$/)
+// console.log(req.keys())
+// console.log(req('./dashboard.svg'))
+// console.log(svg)
 const requireAll = requireContext => requireContext.keys().map(requireContext)
 requireAll(req)
